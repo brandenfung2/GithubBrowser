86d45439ff9c67f3c29d7c218515c57fb982dd71
Jan <stoltmanjan@gmail.com>
2019-01-21 20:27:58 +0100

    Migration to kotlin gradle dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.yggdralisk.githubbrowser"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+    implementation("com.android.support:appcompat-v7:28.0.0")
+
+    //Constraint layout
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha3")
+
+    //Tests
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
+
+repositories {
+    mavenCentral()
+    maven("http://repository.jetbrains.com/all")
+}
