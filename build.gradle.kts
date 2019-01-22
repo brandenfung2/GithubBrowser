6189d3006e6bfccbd30d637f9841964c00bdda3b
Jan <stoltmanjan@gmail.com>
2019-01-22 17:08:29 +0100

    Add gradle-code-quality-tools-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ buildscript {
     repositories {
         google()
         jcenter()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.3.0")
         classpath(kotlin("gradle-plugin", kotlinVersion))
+        classpath("com.vanniktech:gradle-code-quality-tools-plugin:0.15.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
