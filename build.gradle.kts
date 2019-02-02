701bb81e68d673137dc9a17585da0d31a32579f8
Jan <stoltmanjan@gmail.com>
2019-02-02 20:03:03 +0100

    Add jacoco reports generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.3.0")
         classpath(kotlin("gradle-plugin", kotlinVersion))
-        classpath("com.vanniktech:gradle-code-quality-tools-plugin:0.15.0")
+        classpath("com.dicedmelon.gradle:jacoco-android:0.1.3")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
